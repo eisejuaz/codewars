d47eef5a9aa05f24dade199d06e092df4def521d
Franco Gargiulo <animalitof@gmail.com>
2019-03-21 01:07:46 -0300

    soluciones es5 y es6, ver uso de spread operators

--- 7th-kyu/the-highest-profit-wins.js
+++ 7th-kyu/the-highest-profit-wins.js
@@ -30,3 +30,22 @@ const minMax = (arr) => [Math.min(...arr), Math.max(...arr)];
 minMax([1,2,3,4,5]);
 minMax([2334454,5]);
 minMax([1]);
+
+// sample tests
+
+// var test = function(arr, res){
+//     Test.assertSimilar(minMax(arr), res, "tested on " + JSON.stringify(arr) + ":");
+//   };
+  
+//   Test.describe("minMax", function(){
+//     Test.it("should work for some examples", function(){
+//       var i, r;
+//       test([1,2,3,4,5], [1,5]);
+//       test([2334454,5], [5, 2334454]);
+      
+//       for(i = 0; i < 20; ++i){
+//         r = Math.random();
+//         test([r], [r,r]);
+//       }   
+//     });
+//   });
